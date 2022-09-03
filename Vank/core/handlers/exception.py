a58fde644508e7e0d49c2716da21e1c857b6a032
Vank <852003793@qq.com>
2022-09-03 19:07:46 +0800

    hotfix

--- Vank/core/handlers/exception.py
+++ Vank/core/handlers/exception.py
@@ -1,41 +1,6 @@
 from Vank.core.http.status import *
 from Vank.core.http.response import Response
 from Vank.core.exceptions import *
-from peewee import (
-    DataError,
-    InternalError,
-    InterfaceError,
-    DatabaseError,
-    IntegrityError,
-    OperationalError,
-    ProgrammingError,
-    NotSupportedError
-)
-
-
-#
-#
-# def dump_response(data: dict):
-#     return json.dumps(data).encode()
-#
-#
-# def HttpNotFound(request):
-#     code = HTTP_404_NOT_FOUND
-#
-#     error = {
-#         'error': '请求路径有误',
-#         'code': code
-#     }
-#     return Response404(error)
-#
-#
-# def Http_MethodNotAllowed(request):
-#     code = HTTP_405_METHOD_NOT_ALLOWED
-#     error = {
-#         'error': '请求方法不允许',
-#         'code': code
-#     }
-#     return Response405(error)
 
 def default_handler(request, exec):
     if isinstance(exec, NotFoundException):
