d7060c1292db42b73013452621ec355979db2106
Vank <852003793@qq.com>
2022-09-07 14:21:57 +0800

    封装导入模块工具

---
+++ Vank/utils/__init__.py
@@ -0,0 +1,8 @@
+# @FileName: __init__.py
+# @Date    : 2022/9/7-1:53
+# @Author  : Vank
+# @Project : Vank
+
+from Vank.utils.load_module import import_from_str
+
+__all__ = ['import_from_str']
---
+++ Vank/utils/load_module.py
@@ -0,0 +1,18 @@
+# @FileName: load_module.py
+# @Date    : 2022/9/7-1:54
+# @Author  : Vank
+# @Project : Vank
+from importlib import import_module
+
+
+def import_from_str(import_str: str):
+    """
+    导入一个模块中的类或属性
+    :param import_str: 导入路径
+    :return: 类或属性
+    """
+    try:
+        moudle, att_or_class = import_str.rsplit('.', 1)
+        return getattr(import_module(moudle), att_or_class)
+    except Exception as e:
+        raise ImportError(f'导入{import_str}失败,路径应该用.分开 例如 Vank.utils.load_module.import_from_str')
