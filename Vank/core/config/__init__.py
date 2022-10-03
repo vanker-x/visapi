c978cb448cd1e5d36a76a62139d531855236a988
Vank <852003793@qq.com>
2022-10-03 18:04:29 +0800

    hotfix

--- Vank/core/config/__init__.py
+++ Vank/core/config/__init__.py
@@ -6,24 +6,20 @@ ENVIROMENT_VARIABLE_OF_PROJECT = 'PROJECT_SETTING'
 
 class Settings:
     def __init__(self):
-        module = os.environ.get(ENVIROMENT_VARIABLE_OF_PROJECT, '')
+        module = os.environ.get(ENVIROMENT_VARIABLE_OF_PROJECT, None)
         if not module:
-            raise RuntimeError(
+            raise ModuleNotFoundError(
                 "运行失败,运行时未找到settings.py的环境变量 PROJECT_SETTING"
                 "你必须先配置settings.py文件的环境变量"
             )
         try:
             self.module = import_module(module)
         except:
-            raise RuntimeError('加载设置文件失败,请查看是否设置正确')
-
-    def __getattr__(self, item):
-        value = getattr(self.module, item, None)
-        if not value:
-            raise AttributeError(f'没有找到属性:{item}')
-        self.__dict__[item] = value
-
-        return value
+            raise ModuleNotFoundError('加载settings失败,你把项目中的settings.py删除了吗?')
+        for setting_name in dir(self.module):
+            if not setting_name.isupper():
+                continue
+            setattr(self, setting_name, getattr(self.module, setting_name))
 
 
 conf = Settings()
