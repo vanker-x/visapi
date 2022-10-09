3b4968ebebaf0f3573884b9812d99a34cc6a2a8a
Vank <wanke@datagrand.com>
2022-10-09 17:38:34 +0800

    添加跨域中间件  --待完善

---
+++ Vank/middleware/cors.py
@@ -0,0 +1,10 @@
+# Created by Vank
+# DateTime: 2022/10/9-17:33
+# Encoding: UTF-8
+from Vank.middleware.base import BaseMiddleware
+
+
+class CorsMiddleware(BaseMiddleware):
+    """解决跨域问题的中间件"""
+    # TODO 待开发
+    pass
